b80ba8468aa37ab32e96b9d60e3cd1806d672c54
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-24 22:19:39 -0400

    Update App.js
    - passed in data in state variable
    - passed in that variable in the List component
    - pass in the people array into the h3 for the length of the array

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+  const [people, setPeople] = useState(data) //pass in the data array file
   return <main>
     <section className='container'>
-      <h3>0 birthdays today</h3>
-      <List />
+      <h3>{people.length} birthdays today</h3> {/* checks how big the list is which is 5 currently */}
+      <List people={people}/> {/* Add people prop to pass in state */}
       <button onClick={() => console.log('you clicked me')}>
         Clear All
       </button>
