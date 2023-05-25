2bf4361642f1b3d6396610237c0ced634e862379
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-24 22:11:44 -0400

    Further setup (List component & button)
    - add list component
    - add button with empty onClick for setup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return <main>
     <section className='container'>
       <h3>0 birthdays today</h3>
+      <List />
+      <button onClick={() => console.log('you clicked me')}>
+        Clear All
+      </button>
     </section>
     
   </main>;
