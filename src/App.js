e334fdc4fd4d7b55729e083d2c3b95ebb2e565f9
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-24 22:08:04 -0400

    Birthdays container setup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return <main>
+    <section className='container'>
+      <h3>0 birthdays today</h3>
+    </section>
+    
+  </main>;
 }
 
 export default App;
